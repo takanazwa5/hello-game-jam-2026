97a3a7d7ff01fa4ccdb950ed3dd04cd137d48360
takanazwa5 <32649717+takanazwa5@users.noreply.github.com>
2026-08-28 12:02:00 +0200

    Add smooth anemone dirtiness

--- anemone/anemone.gd
+++ anemone/anemone.gd
@@ -6,7 +6,9 @@ const CLEAN_TIME_MAX: float = 20.0
 
 
 var _is_fih_inside: bool = false
-var _is_clean: bool = true
+var _is_clean: bool = false # na true wszystkie maja wait_time z node'a
+var _shader_material: ShaderMaterial
+var _tween: Tween
 
 
 @onready var cleaning_area: Area3D = %CleaningArea
@@ -14,17 +16,18 @@ var _is_clean: bool = true
 @onready var clean_timer: Timer = %CleanTimer
 @onready var death_timer: Timer = %DeathTimer
 @onready var mesh_instance: MeshInstance3D = %anemoneP
+@onready var dirtiness_label: Label3D = %DirtinessLabel
 
 
 func _ready() -> void:
+	_shader_material = mesh_instance.get_surface_override_material(0)
 	var random_type: int = randi_range(1, 4)
 	print("chosen %s for %s" % [random_type, name])
 	var clean_texture_chosen: Texture2D = load("res://anemone/textures/anemone_clean_%s.png" % random_type)
 	var dirty_texture_chosen: Texture2D = load("res://anemone/textures/anemone_dirty_%s.png" % random_type)
-	var shader_material: ShaderMaterial = mesh_instance.get_surface_override_material(0)
-	shader_material.set_shader_parameter(&"TextureClean", clean_texture_chosen)
-	shader_material.set_shader_parameter(&"TextureDirty", dirty_texture_chosen)
-	print("%s has %s\n" % [name, shader_material.get_shader_parameter(&"TextureClean")])
+	_shader_material.set_shader_parameter(&"TextureClean", clean_texture_chosen)
+	_shader_material.set_shader_parameter(&"TextureDirty", dirty_texture_chosen)
+	print("%s has %s\n" % [name, _shader_material.get_shader_parameter(&"TextureClean")])
 
 
 	if not OS.is_debug_build():
@@ -35,7 +38,7 @@ func _ready() -> void:
 	clean_timer.timeout.connect(_on_clean_timer_timeout)
 	death_timer.timeout.connect(_on_death_timer_timeout)
 
-	clean_timer.start(randf_range(CLEAN_TIME_MIN, CLEAN_TIME_MAX))
+	_make_clean()
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -47,6 +50,7 @@ func _unhandled_input(event: InputEvent) -> void:
 func _process(_delta: float) -> void:
 	clean_label.text = "CZYSTY" if _is_clean else "BRUDNY"
 	clean_label.text += " %.2f" % clean_timer.time_left if _is_clean else " %.2f" % death_timer.time_left
+	dirtiness_label.text = "Dirtiness %.2f" % _shader_material.get_shader_parameter(&"Dirtiness")
 
 
 func _make_clean() -> void:
@@ -55,6 +59,10 @@ func _make_clean() -> void:
 	var wait_time: float = clean_timer.wait_time if _is_clean else randf_range(CLEAN_TIME_MIN, CLEAN_TIME_MAX)
 	clean_timer.start(wait_time)
 	_is_clean = true
+	if is_instance_valid(_tween): _tween.kill()
+	_shader_material.set_shader_parameter(&"Dirtiness", 0.0)
+	_tween = create_tween()
+	_tween.tween_property(_shader_material, ^"shader_parameter/Dirtiness", 1.0, wait_time)
 	print("anemone %s cleaned, %.2f sec left" % [self, wait_time])
 
 
